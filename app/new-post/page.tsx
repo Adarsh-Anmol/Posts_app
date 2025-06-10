31edbf22fece6014ce0cd512c5fa57759ee2bfd4
Adarsh Anmol <adarshanmol10@gmail.com>
2025-06-10 17:21:38 +0530

    6: made the fields of the form compulsory, added a post-form client component

--- app/new-post/page.tsx
+++ app/new-post/page.tsx
@@ -1,50 +1,46 @@
-import FormSubmit from "@/components/form-submit";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
+import Postform from "@/components/post-form";
+
+interface PostState {
+  errors?: string[];
+}
+
 
 export default function NewPostPage() {
-  async function createPost(formData: FormData){
+  async function createPost(state : PostState,formData: FormData){
       "use server";
   const title = formData.get('title');
   const image = formData.get('image');
   const content = formData.get('content');
 
-  storePost({
+  const errors:string[] = [];
+  
+  if(!title || title.toString().trim().length === 0){
+    errors.push("Title is Required!");
+  }
+
+  if(!content || content.toString().trim().length === 0){
+    errors.push("Content is Required!");
+  }
+
+  if(!image || !(image instanceof File)||image.size === 0){
+    errors.push("Image is Required!");
+  }
+
+  if(errors.length >0){
+    return {errors};
+  }
+
+  await storePost({
     imageUrl: '',
-    title,
-    content,
+    title : title?.toString(),
+    content : content?.toString(),
     userId:1
   })
 
   redirect('/feed')
   }
 
-
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows={5} />
-        </p>
-        <p className="form-actions">
-          <FormSubmit/>
-        </p>
-      </form>
-    </>
-  );
+  return <Postform action={createPost}/>
 }
