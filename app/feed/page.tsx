5c641ea4a854af7c33e41028187d61c587d92c98
Adarsh Anmol <adarshanmol10@gmail.com>
2025-06-11 18:21:40 +0530

    14; added metadata. static and dynamic

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,6 +1,17 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+export async function generateMetadata(){
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+
+  return{
+    title: `Browse all ${numberOfPosts} posts`,
+    description: 'All the posts in one place'
+  }
+
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
